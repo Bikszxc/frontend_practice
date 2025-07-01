8283f7f009c84359dc74b20c5b561914ca0b09f2
Bikzxcs <reagan2sotelo@gmail.com>
2025-07-02 01:15:06 +0800

    - more functionality

--- quotify/script.js
+++ quotify/script.js
@@ -1,7 +1,11 @@
 class QuotifyApp {
     author;
     constructor() {
+        this.savedQuotes = JSON.parse(localStorage.getItem('quotesSaved')) || [];
+
         this.refreshBtn = document.getElementById("refreshQuote");
+        this.saveBtn = document.getElementById("saveQuote");
+        this.viewBtn = document.getElementById("viewSaved");
 
         this.quoteText = document.getElementById("quote-text");
         this.quoteAuthor = document.getElementById("quote-author");
@@ -11,6 +15,8 @@ class QuotifyApp {
 
     init() {
         this.refreshBtn.addEventListener("click", async () => {await this.refreshQuote()})
+        this.saveBtn.addEventListener("click",  () => {this.saveQuote()})
+        this.viewBtn.addEventListener("click", () => {this.viewSavedQuotes()})
 
         this.refreshQuote()
     }
@@ -34,14 +40,36 @@ class QuotifyApp {
     }
 
     async refreshQuote() {
-
         this.quoteData = await this.getQuote();
 
-        const quote = this.quoteData.text;
-        const author = this.quoteData.author;
+        this.quote = this.quoteData.text;
+        this.author = this.quoteData.author;
 
-        this.quoteText.innerHTML = quote;
-        this.quoteAuthor.innerHTML = author;
+        this.quoteText.innerHTML = this.quote;
+        this.quoteAuthor.innerHTML = "- " + this.author;
+    }
+
+    saveQuote() {
+        if (!this.savedQuotes.some(quote => quote.id === this.quoteData.id)) {
+            this.savedQuotes.unshift(this.quoteData);
+        } else {
+            alert("This quote is already saved!")
+        }
+
+        this.saveToStorage()
+    }
+
+    viewSavedQuotes() {
+        if (this.savedQuotes.length > 0) {
+            console.log(this.savedQuotes);
+        } else {
+            console.log("No saved quotes.");
+        }
+
+    }
+
+    saveToStorage() {
+        localStorage.setItem('quotesSaved', JSON.stringify(this.savedQuotes));
     }
 }
 
